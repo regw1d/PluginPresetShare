52c43a1ead3fc9526d70e87df8089bbe7da72cfd
regw1d <regwid228@gmail.com>
2025-01-25 21:35:43 +0400

    upd for config.py
    deleted constants

--- Bot/PPSB/config.py
+++ Bot/PPSB/config.py
@@ -1,7 +1,7 @@
 # config
-TOKEN = '8049055796:AAEVznkAoC_EPiZEpulsFy3ocgJ_5fTpeI0'
-OWNER_ID = '1723389170'
-BOOSTY = 'https://boosty.to/regw1d'
-DONATE = 'https://www.donationalerts.com/r/regw1d'
-CHANNEL = 'https://t.me/+2oWmBhIhLjw5OWI6'
+TOKEN = ''
+OWNER_ID = ''
+BOOSTY = ''
+DONATE = ''
+CHANNEL = ''
 GITHUB = 'https://github.com/regw1d/PluginPresetShare'

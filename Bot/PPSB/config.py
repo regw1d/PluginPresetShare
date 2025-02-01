3556f46417739cf40be5d4dea1c6338966293eda
MaDeInCCCP2 <61327828+MaDeInCCCP2@users.noreply.github.com>
2025-02-02 03:45:43 +0400

    Update config.py

--- Bot/PPSB/config.py
+++ Bot/PPSB/config.py
@@ -1,7 +1,9 @@
-# config
-TOKEN = ''
-OWNER_ID = ''
-BOOSTY = ''
-DONATE = ''
-CHANNEL = ''
-GITHUB = 'https://github.com/regw1d/PluginPresetShare'
+# config.py
+TOKEN = ''  # Токен бота
+OWNER_ID = ''  # ID владельца бота (должен быть int)
+BOOSTY = 'https://boosty.to/regw1d'  # Ссылка на Boosty
+DONATE = 'https://www.donationalerts.com/r/regw1d'  # Ссылка на донат
+CHANNEL = 'https://t.me/+2oWmBhIhLjw5OWI6'  # Ссылка на канал
+GITHUB = 'https://github.com/regw1d/PluginPresetShare'  # Ссылка на GitHub
+MONGO_URI = 'mongodb://localhost:27017/'  # URI для MongoDB (заменить на реальный)
+DB_NAME = 'ppsb_db'  # Название базы данных

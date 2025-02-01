72a37b00684bbf69fbe993e25239802a9daab729
MaDeInCCCP2 <61327828+MaDeInCCCP2@users.noreply.github.com>
2025-02-02 03:46:07 +0400

    Update handlers.py

--- Bot/PPSB/app/handlers.py
+++ Bot/PPSB/app/handlers.py
@@ -1,44 +1,35 @@
-# handlers
+# handlers.py
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram import Router, types
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from datetime import datetime
-from config import *
-import json
-import os
+from config import GITHUB, DONATE
 
 router = Router()
 
-# /start
 @router.message(CommandStart())
 async def start(message: Message):
     await message.answer(
         'PluginPresetsShareBot (PPSB) is already work! For start work with bot send /quest'
-        )
+    )
 
-# /help
 @router.message(Command('help'))
-async def start(message: Message):
+async def help_command(message: Message):
     await message.answer(
         "Доступные команды бота:\n"
         "/start – Просто тест команда.\n"
         "/help – Страница с подсказками.\n"
         "/github – Ссылка на github проекта.\n"
+        "/donate – Поддержать автора.\n"
         "/contact – Написать отзыв.\n"
         "/quest – Основная команда бота (МНОГО БАГОВ!).\n"
-        )
+    )
 
-# /github
 @router.message(Command('github'))
 async def github(message: Message):
-    await message.answer(
-        GITHUB
-        )
-    
-# /donate 
+    await message.answer(GITHUB)
+
 @router.message(Command('donate'))
 async def donate(message: Message):
-    await message.answer(
-        DONATE
-        ) #why not
+    await message.answer(DONATE)

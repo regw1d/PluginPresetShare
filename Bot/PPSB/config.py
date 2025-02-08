dba139a72327202e385f94d17c453161990e1208
regw1d <regwid228@gmail.com>
2025-02-08 23:56:02 +0400

    Update config.py

--- Bot/PPSB/config.py
+++ Bot/PPSB/config.py
@@ -1,13 +1,13 @@
 # ppsb>config
-TOKEN = '8049055796:AAEVznkAoC_EPiZEpulsFy3ocgJ_5fTpeI0'
-# id owner bot
-OWNER_ID = '1723389170'
+TOKEN = 'token'
+OWNER_ID = 'owner'
 # url's
 BOOSTY = 'https://boosty.to/regw1d'
 DONATE = 'https://www.donationalerts.com/r/regw1d'
 CHANNEL = 'https://t.me/+2oWmBhIhLjw5OWI6'
 GITHUB = 'https://github.com/regw1d/PluginPresetShare'
-WEBAPP_URL = 'https://127.0.0.1:5500'  # Замените на ваш URL веб-приложения
 # mongodb's
 MONGO_URI = "mongodb://localhost:27017"
 DB_NAME = "PPSB"
+# web app's
+WEBAPP_URL = 'https://127.0.0.1:5500'
